ab0a694a0ec95f7109ab03d32d22eb0d60263940
Quinton Jason <quinton.jason@gmail.com>
2026-02-12 10:49:54 -0600

    add js click event

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // JS scripts placed here
+const button = document.querySelector('.submit-1');
+const area = document.querySelector('.area');
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    area.classList.toggle('on');
+});
