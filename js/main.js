31ed5c03647e01e30f9ee722355512ad74cf4327
Quinton Jason <quinton.jason@gmail.com>
2026-03-17 10:00:54 -0500

    adding javascript click events

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
 // JS scripts placed here
-const button = document.querySelector('.submit-1');
-const area = document.querySelector('.area');
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    area.classList.toggle('on');
+// const button = document.querySelector('.submit-1');
+// const area = document.querySelector('.area');
+// button.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     area.classList.toggle('on');
+// });
+
+
+const star = document.querySelector(".star");
+const stars = document.querySelectorAll(".star");
+console.log("hey ");
+console.log(star);
+console.log(stars);
+
+
+const body = document.querySelector('body');
+const pinkfish = document.querySelector(".bl-fish-1");
+const barracudda = document.querySelector(".ba-fish-1");
+const barracudda2 = document.querySelector(".ba-fish-2");
+
+console.log(pinkfish);
+console.log(barracudda);
+
+pinkfish.addEventListener('click', () => {
+// Code to execute when the element is clicked
+    body.classList.toggle("pink");
+});
+
+barracudda.addEventListener('click', () => {
+// Code to execute when the element is clicked
+    barracudda.classList.toggle("flipped");
+    body.classList.toggle('invert-colors')
+    pinkfish.classList.toggle("hvr-bob");
+});
+
+barracudda2.addEventListener('click', () => {
+// Code to execute when the element is clicked
+    barracudda.classList.toggle("swim");
 });
